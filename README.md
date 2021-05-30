c761fc0ddf86e5ad720a223d884625bb6330439c
omchakrabarty <omchakrabarty@gmail.com>
2021-05-30 14:25:14 +0530

    Code update

--- README.md
+++ README.md
@@ -17,4 +17,65 @@ If you use the codes and models from this repo, please cite our work. Thanks!
 }
 ```
 
+### Prerequisites
+- Python 3.X
+- PyTorch 1.X
+- torchvision
+- pyyaml
+
+For video data pre-processing, you may need ffmpeg.
+
+
+## Requirements
+```
+pip install -r requirements.txt
+```
+
+## Python script overview
+
+main.py - It contains the code for Temporal Contrastive Learning(TCL) with the 2 pathway model.
+
+opts.py - It contains the file with default value for different parameter used in 2 pathway model.
+
+ops/dataset_config.py - It contains the code for different config for different dataset and their location e.g Kinetics, Jester, SomethingV2
+
+ops/dataset.py - It contains the code for how frames are sampled from video
+
+### Key Parameters:
+ use_group_contrastive: to use group contrastive loss \
+ use_finetuning : option to use finetuning at the last \
+ finetune_start_epoch: from which epoch to start finetuning \
+ finetune_lr: if want to use different lr other than normal one\
+ gamma_finetune: weight for pl_loss in finetuning step \
+ finetune_stage_eval_freq: printing freq for finetuning stage
+ threshold: used in fine tuning step for selection of labels \
+ sup_thresh: till which epoch supervised only to be run \
+ percentage: percentage of unlabeled data e.g 0.99 ,0.95 \
+ gamma: weight of instance contrastive loss \
+ lr: starting learning rate \
+ mu: ratio of unlabeled to labeled data \
+ flip: whether to use horizontal flip in transforms or not
+
+
+### Creating Dataset
+The `root_dataset.yaml` must contains the root folder of the particular dataset.
+Each dataset folder must contains two sub-folders.
+- Frames: This sub-folder contains the extracted Frames of the videos from the dataset. To extract frames from videos, Please refer to [TSM](https://github.com/mit-han-lab/temporal-shift-module#data-preparation)
+- data: This sub-folder contains the training,validation split files along with the file containing the list of all the classes of the videos.These names should be consistent with `ops/dataset_config.py`. Samples files are provided in the dataset folder.
+
+### Training TCL
+- For running `x%` labeled data scenario, it expects to have a folder named `Run_x` where all the labeled and unlabeled data will be split as per the input seed.
+- All the models and logs will be stored inside a sub folder of checkpoints directory. A different subfolder will be created on each execution.
+
+### Sample Code to Run for 2 stream final approach:
+
+`python main.py somethingv2 RGB --seed 123 --strategy classwise
+ --arch resnet18 --num_segments 8 --second_segments 4 --threshold 0.8 --gd 20 --lr 0.02 --wd 1e-4 
+ --epochs 400 --percentage 0.95 --batch-size 8 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 --print-freq 50
+ --shift --shift_div=8 --shift_place=blockres --npb --gpus 0 1  --mu 3 --gamma 9 --gamma_finetune 1 
+--use_group_contrastive --use_finetuning --finetune_start_epoch 350 --sup_thresh 50 --valbatchsize 16 --finetune_lr 0.002`
+
+
+
+
 Thanks to TSM
---
+++ ops/dataset.py
@@ -0,0 +1,198 @@
+import torch.utils.data as data
+
+from PIL import Image
+import os
+import numpy as np
+from numpy.random import randint
+import torch
+import random
+from ops.transforms import *
+from torchvision import transforms
+
+class VideoRecord(object):
+    def __init__(self, row):
+        self._data = row
+
+    @property
+    def path(self):
+        return self._data[0]
+
+    @property
+    def num_frames(self):
+        return int(self._data[1])
+
+    @property
+    def label(self):
+        return int(self._data[2])
+
+
+class TSNDataSet(data.Dataset):
+    def __init__(self, root_path, list_file, unlabeled= False,second_segments=2,
+                 num_segments=3, new_length=1, modality='RGB',
+                 image_tmpl='img_{:05d}.jpg', transform=None,
+                 random_shift=True, test_mode=False,
+                 remove_missing=False, dense_sample=False, twice_sample=False):
+
+        self.root_path = root_path
+        self.list_file = list_file
+        self.num_segments = num_segments
+        self.unlabeled = unlabeled
+        self.new_length = new_length
+        self.second_segments = second_segments
+        self.modality = modality
+        self.image_tmpl = image_tmpl
+        self.transform = transform
+        self.random_shift = random_shift
+        self.test_mode = test_mode
+        self.remove_missing = remove_missing
+        self.dense_sample = dense_sample  # using dense sample as I3D
+        self.twice_sample = twice_sample  # twice sample for more validation
+        if self.dense_sample:
+            print('=> Using dense sample for the dataset...')
+        if self.twice_sample:
+            print('=> Using twice sample for the dataset...')
+
+        if self.modality == 'RGBDiff':
+            self.new_length += 1  # Diff needs one more image to calculate diff
+
+        self._parse_list()
+
+    def _load_image(self, directory, idx):
+        if self.modality == 'RGB' or self.modality == 'RGBDiff':
+            try:
+                return [Image.open(os.path.join(self.root_path, directory, self.image_tmpl.format(idx))).convert('RGB')]
+            except Exception:
+                print('error loading image:', os.path.join(self.root_path, directory, self.image_tmpl.format(idx)))
+                return [Image.open(os.path.join(self.root_path, directory, self.image_tmpl.format(1))).convert('RGB')]
+
+    def _parse_list(self):
+        # check the frame number is large >3:
+        tmp = [x.strip().split(' ') for x in open(self.list_file)]
+        if not self.test_mode or self.remove_missing:
+            tmp = [item for item in tmp if int(item[1]) >= 3]
+        self.video_list = [VideoRecord(item) for item in tmp]
+
+        if self.image_tmpl == '{:06d}-{}_{:05d}.jpg':
+            for v in self.video_list:
+                v._data[1] = int(v._data[1]) / 2
+        print('video number:%d' % (len(self.video_list)))
+
+    def _sample_indices(self, record,num_segments):
+        """
+
+        :param record: VideoRecord
+        :return: list
+        """
+        if self.dense_sample:  # i3d dense sample
+            sample_pos = max(1, 1 + record.num_frames - 64)
+            t_stride = 64 // num_segments
+            start_idx = 0 if sample_pos == 1 else np.random.randint(0, sample_pos - 1)
+            offsets = [(idx * t_stride + start_idx) % record.num_frames for idx in range(num_segments)]
+            return np.array(offsets) + 1
+        else:  # normal sample
+            average_duration = (record.num_frames - self.new_length + 1) // num_segments
+            if average_duration > 0:
+                offsets = np.multiply(list(range(num_segments)), average_duration) + randint(average_duration,
+                                                                                                  size=num_segments)
+            elif record.num_frames > num_segments:
+                offsets = np.sort(randint(record.num_frames - self.new_length + 1, size=num_segments))
+            else:
+                offsets = np.zeros((num_segments,))
+            return offsets + 1
+
+    def _get_val_indices(self, record,num_segments):
+        if self.dense_sample:  # i3d dense sample
+            sample_pos = max(1, 1 + record.num_frames - 64)
+            t_stride = 64 // num_segments
+            start_idx = 0 if sample_pos == 1 else np.random.randint(0, sample_pos - 1)
+            offsets = [(idx * t_stride + start_idx) % record.num_frames for idx in range(num_segments)]
+            return np.array(offsets) + 1
+        else:
+            if record.num_frames > num_segments + self.new_length - 1:
+                tick = (record.num_frames - self.new_length + 1) / float(num_segments)
+                offsets = np.array([int(tick / 2.0 + tick * x) for x in range(num_segments)])
+            else:
+                offsets = np.zeros((num_segments,))
+            #print("length of no of val frames is {}".format(offsets+1))
+            return offsets + 1
+
+    def _get_test_indices(self, record,num_segments):
+        if self.dense_sample:
+            sample_pos = max(1, 1 + record.num_frames - 64)
+            t_stride = 64 // num_segments
+            start_list = np.linspace(0, sample_pos - 1, num=10, dtype=int)
+            offsets = []
+            for start_idx in start_list.tolist():
+                offsets += [(idx * t_stride + start_idx) % record.num_frames for idx in range(num_segments)]
+            return np.array(offsets) + 1
+        elif self.twice_sample:
+            tick = (record.num_frames - self.new_length + 1) / float(num_segments)
+
+            offsets = np.array([int(tick / 2.0 + tick * x) for x in range(num_segments)] +
+                               [int(tick * x) for x in range(num_segments)])
+            #print("length of no of test frames is {}".format(offsets+1))
+
+            return offsets + 1
+        else:
+            tick = (record.num_frames - self.new_length + 1) / float(num_segments)
+            offsets = np.array([int(tick / 2.0 + tick * x) for x in range(num_segments)])
+            #print("length of no of test frames is {}".format(offsets+1))
+            return offsets + 1
+
+    def __getitem__(self, index):
+        record = self.video_list[index]
+        # check this is a legit video folder
+        if self.image_tmpl == '{:06d}-{}_{:05d}.jpg':
+            file_name = self.image_tmpl.format(int(record.path), 'x', 1)
+            full_path = os.path.join(self.root_path, '{:06d}'.format(int(record.path)), file_name)
+        else:
+            file_name = self.image_tmpl.format(1)
+            full_path = os.path.join(self.root_path, record.path, file_name)
+
+        while not os.path.exists(full_path):
+            print('################## Not Found:', os.path.join(self.root_path, record.path, file_name))
+            index = np.random.randint(len(self.video_list))
+            record = self.video_list[index]
+            if self.image_tmpl == '{:06d}-{}_{:05d}.jpg':
+                file_name = self.image_tmpl.format(int(record.path), 'x', 1)
+                full_path = os.path.join(self.root_path, '{:06d}'.format(int(record.path)), file_name)
+            else:
+                file_name = self.image_tmpl.format(1)
+                full_path = os.path.join(self.root_path, record.path, file_name)
+
+        if not self.test_mode:
+            if self.random_shift:
+                if self.unlabeled:
+                    segment_indices_fast = self._sample_indices(record,self.num_segments)
+                    segment_indices_slow = self._sample_indices(record, self.second_segments)
+                    #print ("length of fast segment_indices_fast {}".format(segment_indices_fast))
+                    #print("length of slow segment_indices_slow {}". format(segment_indices_slow))
+                    fast_data,_ = self.get(record, segment_indices_fast)
+                    slow_data,_= self.get(record,segment_indices_slow)
+                    return fast_data,slow_data
+
+
+                if not self.unlabeled:
+                    segment_indices = self._sample_indices(record,self.num_segments)
+            else:
+                segment_indices = self._get_val_indices(record,self.num_segments)
+        else:
+            segment_indices = self._get_test_indices(record,self.num_segments)
+        return self.get(record, segment_indices)
+
+    def get(self, record, indices):
+
+        images = list()
+        for seg_ind in indices:
+            p = int(seg_ind)
+            for i in range(self.new_length):
+                seg_imgs = self._load_image(record.path, p)
+                images.extend(seg_imgs)
+                if p < record.num_frames:
+                    p += 1
+
+        process_data = self.transform(images)
+        return process_data, record.label
+
+    def __len__(self):
+        return len(self.video_list)
